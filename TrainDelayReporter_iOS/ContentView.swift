5b30c5a300de7ddced4f7359a42cdf19b68bd4a2
RyuseiNomi <kanontonto1010@gmail.com>
2020-04-11 16:02:24 +0900

    Initial Commit

---
+++ TrainDelayReporter_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrainDelayReporter_iOS
+//
+//  Created by 能美龍星 on 2020/04/11.
+//  Copyright © 2020 Ryusei Nomi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
