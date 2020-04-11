bb60e8e2e60f5828360b1a5f3cbf1425417bbb05
RyuseiNomi <kanontonto1010@gmail.com>
2020-04-11 17:50:05 +0900

    import QGrid and show delay list by using it

--- TrainDelayReporter_iOS/UI/Components/TrainRouteCell.swift
+++ TrainDelayReporter_iOS/UI/Components/TrainRouteCell.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Ryusei Nomi. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+// Struct which has info about each train route
+struct TrainRoute: Identifiable {
+    var id = UUID()
+    let companyName: String
+    let routeName: String
+}
+
+// The cell which has train route info
+struct TrainRouteCell: View {
+    var delayList: TrainRoute
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(delayList.companyName)
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+            Text(delayList.routeName)
+                .foregroundColor(.white)
+                .fontWeight(.heavy)
+                .font(.title)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+        .background(Color.green)
+        .cornerRadius(10)
+    }
+}
