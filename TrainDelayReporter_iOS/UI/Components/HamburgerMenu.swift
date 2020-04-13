4afd717f3642395bbeb573307435479d74a562a4
RyuseiNomi <kanontonto1010@gmail.com>
2020-04-13 22:39:38 +0900

    Create HamburgerMenu View file

---
+++ TrainDelayReporter_iOS/UI/Components/HamburgerMenu.swift
@@ -0,0 +1,19 @@
+//
+//  HamburgerMenu.swift
+//  TrainDelayReporter_iOS
+//
+//  Created by 能美龍星 on 2020/04/13.
+//  Copyright © 2020 Ryusei Nomi. All rights reserved.
+//
+
+import SwiftUI
+
+struct HamburgerMenu: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("地域を選択")
+            Text("北海道")
+            Text("東日本")
+        }
+    }
+}
