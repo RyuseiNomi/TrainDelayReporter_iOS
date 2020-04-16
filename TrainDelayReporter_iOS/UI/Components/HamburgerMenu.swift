9a60afa87d89d2360332a9fc1cc4809bcf0116e8
RyuseiNomi <kanontonto1010@gmail.com>
2020-04-16 23:40:11 +0900

    fix hamburger menu region texts

--- TrainDelayReporter_iOS/UI/Components/HamburgerMenu.swift
+++ TrainDelayReporter_iOS/UI/Components/HamburgerMenu.swift
@@ -7,27 +7,47 @@
 //
 
 import SwiftUI
+import QGrid
+
+struct Region: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+struct RegionCell:View {
+    var region: Region
+    var body: some View {
+        ZStack {
+            HStack {
+                Text(self.region.name)
+            }
+        }
+    }
+}
 
 struct HamburgerMenu: View {
+    var regionStructs:[Region] = []
+    let regions:[String] = ["北海道", "東日本", "西日本", "東海", "四国", "九州", "私鉄"]
+    init() {
+        for region in regions {
+            regionStructs.append(Region(name: region as! String))
+        }
+    }
+    
     var body: some View {
         GeometryReader { geometry in
             VStack(alignment: .leading) {
                 Text("地域を選択")
                     .font(.system(size: 24))
                 Divider()
-                ScrollView(.vertical, showsIndicators: true) {
-                    Text("北海道")
-                        .font(.system(size: 18))
-                    Text("東日本")
-                        .font(.system(size: 18))
-                    Text("西日本")
-                        .font(.system(size: 18))
-                    Text("東海")
-                        .font(.system(size: 18))
-                    Text("四国")
-                        .font(.system(size: 18))
-                    Text("九州")
-                        .font(.system(size: 18))
+                QGrid(self.regionStructs,
+                      columns: 1,
+                      vSpacing: 20,
+                      hSpacing: 20,
+                      vPadding: 20,
+                      hPadding: 0
+                ) { region in
+                    RegionCell(region: region)
                 }
             }
             .padding(.horizontal, 20)
