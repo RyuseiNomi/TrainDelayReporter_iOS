27658c892ab59eb5ba36dac04e5fb6e242ec28ed
RyuseiNomi <kanontonto1010@gmail.com>
2020-05-02 22:42:48 +0900

    revise hamburger menu

--- TrainDelayReporter_iOS/UI/Components/HamburgerMenu.swift
+++ TrainDelayReporter_iOS/UI/Components/HamburgerMenu.swift
@@ -10,34 +10,60 @@ import SwiftUI
 import QGrid
 import Combine
 
+struct Menu: Identifiable {
+    var id = UUID()
+    var IconName: String
+    var MenuTitle: String
+}
+
+struct MenuCell: View {
+    var menu: Menu
+    
+    var body: some View {
+        HStack() {
+            Image(menu.IconName)
+                .resizable()
+                .scaledToFit()
+                .padding([.horizontal, .top], 7)
+            Text(menu.MenuTitle)
+        }
+    }
+}
+
 struct HamburgerMenu: View {
     
     @State var isComplete:Bool = false
-    @State var regionStructs:[Region] = []
+    @State var menuCells:[Menu] = []
     
     var body: some View {
-        VStack {
-            Text("地域を選択")
-//                .font(.system(size: 30))
-//                .foregroundColor(.black)
-//                .fontWeight(.bold)
-//            Divider()
-//            if self.isComplete == false {
-//                Text("地域情報を取得中")
-//            }
-//            QGrid(self.regionStructs,
-//                  columns: 1,
-//                  vSpacing: 20,
-//                  hSpacing: 20,
-//                  vPadding: 20,
-//                  hPadding: 0
-//            ) { region in
-//                RegionCell(region: region)
-//            }
+        VStack(alignment: .center) {
+            Text("メニュー")
+            Divider()
+            if self.isComplete == false {
+                Text("メニューを設定中")
+            }
+            QGrid(self.menuCells,
+                  columns: 1,
+                  vSpacing: 20,
+                  hSpacing: 20,
+                  vPadding: 20,
+                  hPadding: 0
+            ) { menu in
+                MenuCell(menu: menu)
+            }
+            
         }
-//        .background(Color(red: 255/255, green: 255/255, blue: 255/255))
-//        .onAppear(perform: {
-//            self.setRegions()
-//        })
+        .background(Color(red: 255/255, green: 255/255, blue: 255/255))
+        .onAppear(perform:{ self.setMenuList() })
+    }
+    
+    public func setMenuList() {
+        self.isComplete = false
+        self.menuCells = []
+        self.menuCells.append(Menu(IconName: "Search-gray", MenuTitle: "駅名から検索"))
+        self.menuCells.append(Menu(IconName: "Region-gray", MenuTitle: "地域から検索"))
+        DispatchQueue.main.async {
+            self.isComplete = true
+        }
     }
 }
