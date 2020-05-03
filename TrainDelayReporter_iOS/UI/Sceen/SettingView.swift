beeb02a3965d909e0bcae3a987697cd63a4405d4
RyuseiNomi <kanontonto1010@gmail.com>
2020-05-03 17:31:11 +0900

    add setting view

---
+++ TrainDelayReporter_iOS/UI/Sceen/SettingView.swift
@@ -0,0 +1,25 @@
+//
+//  SettingView.swift
+//  TrainDelayReporter_iOS
+//
+//  Created by 能美龍星 on 2020/05/03.
+//  Copyright © 2020 Ryusei Nomi. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct SettingView: View {
+    
+    let icon8Url = URL(string: "https://icons8.com")!
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("All of Icons by ")
+                Button(action : { UIApplication.shared.open(self.icon8Url) }) {
+                    Text("Icon8")
+                }
+            }
+        }
+    }
+}
